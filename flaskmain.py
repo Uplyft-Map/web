10cae365331e84470cd988dc1e5342a6b6a7cc01
rhansolo <robinh@andrew.cmu.edu>
2020-02-15 00:38:34 -0500

    hello

--- flaskmain.py
+++ flaskmain.py
@@ -7,14 +7,6 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route('/templates/<path:path>')
-def send_static_files(path):
-    return send_from_directory('templates', path)
-
-@app.route('/bar')
-def barGraph():
-    return render_template("bar.html")
-
 
 if __name__ == "__main__":
     app.debug = True
